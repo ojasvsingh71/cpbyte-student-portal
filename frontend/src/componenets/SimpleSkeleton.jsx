876f88c91b41f207796ba2240398d5a039b0d55a
Ashish Sharma <ashishtawniya1@gmail.com>
2025-07-08 00:09:17 +0530

    added skeleton feature on leaderBoard

---
+++ frontend/src/componenets/SimpleSkeleton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./SimpleSkeleton.css";
+
+const SimpleSkeleton = () => (
+  <div className="w-full p-4 lg:p-12 pt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    {[...Array(3)].map((_, i) => (
+      <div key={i} className="p-4 rounded-lg shadow-lg" style={{ backgroundColor: '#050301' }}>
+        <div className="skeleton h-6 w-32 mb-4"></div>
+        <div className="skeleton h-36 w-full mb-4"></div>
+        <div className="skeleton h-4 w-full mb-2"></div>
+        <div className="skeleton h-4 w-2/3"></div>
+      </div>
+    ))}
+    <div className="col-span-full mt-10">
+      <div className="skeleton h-10 w-1/2 mb-4"></div>
+      {[...Array(5)].map((_, i) => (
+        <div key={i} className="skeleton h-4 w-full mb-2"></div>
+      ))}
+    </div>
+  </div>
+);
+
+export default SimpleSkeleton;
--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -4,7 +4,7 @@ import noimage from "../../public/noImage.webp";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getAll } from "../redux/slices/Leaderboard";
-import { FiLoader } from "react-icons/fi";
+import SimpleSkeleton from "../componenets/SimpleSkeleton";
 
 const Leaderboard = () => {
   const dispatch = useDispatch();
@@ -27,7 +27,9 @@ const Leaderboard = () => {
         <h1 className="text-4xl font-extrabold my-4 text-center text-white drop-shadow-lg">
           Leaderboard🚀
         </h1>
-        {!loading && <div className="w-full p-4 lg:p-12 pt-4">
+        {loading ? (
+  <SimpleSkeleton />
+) : ( <div className="w-full p-4 lg:p-12 pt-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5 w-full">
             {data?.map(
               (item, index) =>
@@ -164,10 +166,8 @@ const Leaderboard = () => {
               </tbody>
             </table>
           </div>
-        </div>}
-        {
-          loading && <div className="w-full mt-16 flex justify-center items-center"><FiLoader size={40} color='white' className='animate-spin'/></div>
-        }
+        </div>)}
+       
       </div>
     </div>
   );

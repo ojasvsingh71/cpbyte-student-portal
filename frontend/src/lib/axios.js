78a8ce2413d40e487236edcdb99f4ee7407fef1e
Harshit Saini <saini457harshit@gmail.com>
2025-08-04 23:31:12 +0530

    readme and axios updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const backendport = import.meta.env.VITE_BACKEND_PORT || 8080;
+console.log("Backend URL:", backendport);
 export const axiosInstance = axios.create({
-  baseURL: "https://cpbyte-student-portal.onrender.com/api/v1",
+  baseURL: `http://localhost:${backendport}/api/v1`,
   withCredentials: true,
 });

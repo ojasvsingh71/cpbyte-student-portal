637d51a49cacb18ccf4308e840b6315d74c97bbf
harshgupta751 <harshachieve300@gmail.com>
2025-09-22 11:34:36 +0530

    Updated import export syntax

--- backend/utils/authTokens.js
+++ backend/utils/authTokens.js
@@ -1,19 +1,19 @@
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
 const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET;
-const ACCESS_TOKEN_EXPIRES = '30m'; 
-const REFRESH_TOKEN_DAYS = 30;
+export const ACCESS_TOKEN_EXPIRES = '30m'; 
+export const REFRESH_TOKEN_DAYS = 30;
 
-function generateRefreshToken() {
+export function generateRefreshToken() {
   return crypto.randomBytes(64).toString('hex'); 
 }
-function hashToken(token) {
+export function hashToken(token) {
   return crypto.createHash('sha256').update(token).digest('hex');
 }
-function generateAccessToken(user) {
+export function generateAccessToken(user) {
   return jwt.sign(    { userId: user.id, library_id: user.library_id, role: user.role },
     ACCESS_TOKEN_SECRET,
     { expiresIn: ACCESS_TOKEN_EXPIRES });
 }
-module.exports = { generateRefreshToken, hashToken, generateAccessToken, REFRESH_TOKEN_DAYS, ACCESS_TOKEN_EXPIRES};
+
